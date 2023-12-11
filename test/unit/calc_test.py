2303c8f6ebf76d13bdc7cf89b056e168f5d4983d
unknown <cristian.perez060@comunidadunir.net>
2023-12-11 04:47:43 +0100

    3r commit rama develop

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -40,11 +40,25 @@ class TestCalculate(unittest.TestCase):
         self.assertRaises(TypeError, self.calc.divide, 2, "2")
         self.assertRaises(TypeError, self.calc.divide, "2", "2")
 
+    def test_divide_method_returns_correct_result(self):
+        self.assertEqual(1, self.calc.divide(2, 2))
+        self.assertEqual(-1, self.calc.divide(2, -2))
+        self.assertEqual(-1, self.calc.divide(-2, 2))
+        self.assertRaises(TypeError, self.calc.divide, 1, 0)
+        self.assertRaises(TypeError, self.calc.divide, "0", 0)
+
     def test_multiply_method_returns_correct_result(self):
         self.assertEqual(4, self.calc.multiply(2, 2))
         self.assertEqual(0, self.calc.multiply(1, 0))
         self.assertEqual(0, self.calc.multiply(-1, 0))
         self.assertEqual(-2, self.calc.multiply(-1, 2))
+        self.assertRaises(TypeError, self.calc.multiply, "0", 0)
+    
+    def test_multiply_method_returns_correct_result(self):
+        self.assertEqual(4, self.calc.multiply(2, 2))
+        self.assertEqual(-4, self.calc.multiply(2, -2))
+        self.assertEqual(-4, self.calc.multiply(-2, 2))
+        self.assertEqual(0, self.calc.multiply(1, 0))
         self.assertRaises(TypeError, self.calc.multiply, "0", 0)
         
     def test_power_method_returns_correct_result(self):
